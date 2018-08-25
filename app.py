55f56e41a5bcbfa8d9e5b3926244defb9eab2c8a
ligo <semal551@gmail.com>
2018-08-25 21:01:13 +0600

    fix bug with the error requesting from opendota.com

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from ligo.requests import heroes, matches
 
 db = DB(config.conn_info)
 
-count_of_requests = 1000
+count_of_requests = 10000
 
 hero_list = heroes.hero_names()
 
-last_match = "4079216614"
+last_match = "4070119619"
 for i in range(count_of_requests):
     print("Iteration ", i+1)
 
--- ligo/requests/matches.py
+++ ligo/requests/matches.py
@@ -16,32 +16,35 @@ def create_table():
 
 
 def get(last_match=""):
-    matches = requests.get(config.opendota_api_url + "publicMatches", params={'less_than_match_id':last_match}).json()
-    db = DB(config.conn_info)
+    try:
+        matches = requests.get(config.opendota_api_url + "publicMatches", params={'less_than_match_id':last_match}).json()
+        db = DB(config.conn_info)
 
-    db.builder.table('heroes').select()
-    heroes_db = db.execute()
-    heroes = {}
-    for hero in heroes_db:
-        heroes[hero[0]] = hero[2]
+        db.builder.table('heroes').select()
+        heroes_db = db.execute()
+        heroes = {}
+        for hero in heroes_db:
+            heroes[hero[0]] = hero[2]
 
-    insert_matches = list()
-    for match in matches:
-        match['winner'] = 0 if match['radiant_win'] == 1 else 1
-        insert_match = {
-            'match_id' : match['match_id'],
-            'start_time': match['start_time'],
-            'duration': match['duration'],
-            'winner': match['winner']
-        }
-        radiant_team = map(int, match['radiant_team'].split(','))
-        for radiant_hero in radiant_team:
-            insert_match[heroes[radiant_hero]] = 0
-        dire_team = map(int, match['dire_team'].split(','))
-        for dire_hero in dire_team:
-            insert_match[heroes[dire_hero]] = 1
-        insert_matches.append(insert_match)
-    return insert_matches
+        insert_matches = list()
+        for match in matches:
+            match['winner'] = 0 if match['radiant_win'] == 1 else 1
+            insert_match = {
+                'match_id' : match['match_id'],
+                'start_time': match['start_time'],
+                'duration': match['duration'],
+                'winner': match['winner']
+            }
+            radiant_team = map(int, match['radiant_team'].split(','))
+            for radiant_hero in radiant_team:
+                insert_match[heroes[radiant_hero]] = 0
+            dire_team = map(int, match['dire_team'].split(','))
+            for dire_hero in dire_team:
+                insert_match[heroes[dire_hero]] = 1
+            insert_matches.append(insert_match)
+        return insert_matches
+    except Exception:
+        return get(last_match)
 
 
 def truncate():
